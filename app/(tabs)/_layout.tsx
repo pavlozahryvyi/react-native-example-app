485792b4ca806427c3e8541c972429af5b289ea6
Pasha <zagriviy2@gmail.com>
2025-06-17 14:56:59 +0300

    styled for header and tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,9 +4,17 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs
-     screenOptions={{
-        tabBarActiveTintColor: '#ffd33d',
-      }}
+      screenOptions={{
+    tabBarActiveTintColor: '#ffd33d',
+    headerStyle: {
+      backgroundColor: '#25292e',
+    },
+    headerShadowVisible: false,
+    headerTintColor: '#fff',
+    tabBarStyle: {
+      backgroundColor: '#25292e',
+    },
+  }}
     >
       <Tabs.Screen name="index" options={{
           title: 'Home',
